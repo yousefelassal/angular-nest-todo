b23f53c898f7fe4e143e8917776733a0ffb8dfef
Yousef Elassal <yousefsameh24@gmail.com>
2024-07-31 12:05:32 +0300

    import and use google icons

--- client/src/app/layout/layout.component.ts
+++ client/src/app/layout/layout.component.ts
@@ -8,19 +8,21 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   template:`
     <div class="sticky top-0 left-0 flex flex-col gap-1 h-screen w-[200px] py-8 px-4">
       <a
-        class="p-2 rounded-md w-full hover:bg-gray-200/80 transition-colors"
+        class="flex gap-2 items-center p-2 rounded-md w-full text-gray-700 hover:bg-gray-200/80 transition-all"
         routerLinkActive="bg-gray-200"
         [routerLinkActiveOptions]="{exact: true}"
         routerLink="/"
       >
+      <span class="material-symbols-outlined">home</span>
         Home
       </a>
       <a
-        class="p-2 rounded-md w-full hover:bg-gray-200/80 transition-colors"
+        class="flex gap-2 items-center p-2 rounded-md w-full text-gray-700 hover:bg-gray-200/80 transition-all"
         routerLinkActive="bg-gray-200"
         [routerLinkActiveOptions]="{exact: true}"
         routerLink="/activity"
       >
+      <span class="material-symbols-outlined">history</span>
         Activity
       </a>
     </div>
